02cb4b015f8215c5d9e54fad15a233793b26f813
Steve <stna7972@colorado.edu>
2022-11-21 16:20:09 -0700

    Add project files.

---
+++ SearchBarBug/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace SearchBarBug;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    private void ListView_ItemTapped(object sender, ItemTappedEventArgs e)
+    {
+        searchBar.Unfocus();
+    }
+}
+
